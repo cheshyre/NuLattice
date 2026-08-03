dcf931800ba820e4a36079c99db03bff3f23a918
Vivek Booshan <vivek@m4pro.local>
2026-08-03 11:50:25 -0400

    add bare minimum for functional hartree fock migration

--- NuLattice/HF/hartree_fock.py
+++ NuLattice/HF/hartree_fock.py
@@ -1,307 +1,182 @@
-"""
-functions to perform a Hartree-Fock computation on the lattice
-"""
-__authors__   =  "Thomas Papenbrock"
-__credits__   =  ["Thomas Papenbrock"]
-__copyright__ = "(c) Thomas Papenbrock"
-__license__   = "BSD-3-Clause"
-__date__      = "2025-07-26"
+from functools import partial
 
-import numpy as np
-from opt_einsum import contract
-#from numba import njit
+import jax
+import jax.numpy as jnp
 
-def get_1body_matrix(myTkin,nstat,dtype=float):
-    """
-    takes the list of one-body matrix elements and turns it into a square matrix
-    
-    :param nstat:  dimension of matrix, i.e. the number of 1-body states
-    :type nstat:   int
-    :param dtype:  data type of returned object
-    :type dtype:   numpy.dtype, i.e. np.float64 or float or np.complex128 or complex
-    :param myTkin: list of one-body matrix elements [[p1,q1,value1], [p2,q2,value2], ...]
-    :type myTkin:  list[list[int,int, float]]
-    :return:       nstat x nstat matrix of the list of matrix elements
-    :rtype:        numpy.array((:,:), dtype=float)
-    """
-    op1 = np.zeros((nstat,nstat),dtype=dtype)
-    for [a, b, val] in myTkin:
-        op1[a,b]+=val
-    return op1
+from NuLattice.utils._jax_types import OneBodyOperator, TwoBodyOperator, ThreeBodyOperator
 
-def contract_2nf(v2,dens):
-    """
-    takes list of two-body matrix elements and contracts them with the density to get a one-body operator
+from .subspace_solver import _occupied_orbitals
 
-    :param v2:   list of two-body matrix elements [p,q,r,s,value] 
-    :type v2:    list[list[int,int,int,int, float]]
-    :param dens: square density matrix
-    :type dens:  numpy.array((:,:), dtype=float)
-    :return:     one-body operator of the same shape as the density matrix dens
-    :rtype:      numpy.array((:,:), dtype=float)
-    """
-    res = np.zeros_like(dens)
-    num_ele = len(v2)
-    for i in range(num_ele):
-        [a, b, c, d, val] = v2[i]
-        
-        res[a,c] += val*dens[b,d] #1
-        res[b,c] -= val*dens[a,d] #P(ab)
-        res[a,d] -= val*dens[b,c] #P(cd)
-        res[b,d] += val*dens[a,c] #P(ab)P(cd) 
+Array = jax.Array
+
+def _adjoint(x):
+    return jnp.swapaxes(jnp.conj(x), -1, -2)
+
+def hermitianize(x):
+    return 0.5 * (x + _adjoint(x))
+
+@jax.jit
+def contract_2nf_fused(indices: Array, values: Array, dens: Array) -> Array:
+    """Contract the sparse two-body interaction with a one-body density."""
+    p, q, r, s = (indices[:, i] for i in range(4))
+    n = dens.shape[0]
+    dtype = jnp.result_type(values.dtype, dens.dtype)
+    res = jnp.zeros((n, n), dtype=dtype)
+    res = res.at[p, r].add(+values * dens[q, s])
+    res = res.at[q, r].add(-values * dens[p, s])
+    res = res.at[p, s].add(-values * dens[q, r])
+    res = res.at[q, s].add(+values * dens[p, r])
     return res
 
-def contract_3nf(w3,dens):
-    """
-    takes list of three-body matrix elements and contracts them with the density to get a one-body operator
 
-    :param w3:   list of two-body matrix elements [p,q,r,s,value] 
-    :type w3:    list[list[int,int,int,int,int,int, float]]
-    :param dens: square density matrix
-    :type dens:  numpy.array((:,:), dtype=float)
-    :return:     one-body operator of the same shape as the density matrix dens
-    :rtype:      numpy.array((:,:), dtype=float)
-    """
-    res = np.zeros_like(dens)
-    for mat_ele in w3:  # we need all 36 antisymmetric combinations of the ket (abc) and bra (def) single-particle states
-        [a, b, c, d, e, f, val] = mat_ele
-        res[a,d] += val*( dens[b,e]*dens[c,f]  # (abc), (def), antisym last two pairs
-                         -dens[c,e]*dens[b,f]
-                         -dens[b,f]*dens[c,e]
-                         +dens[c,f]*dens[b,e] )
-        res[b,d] += val*( dens[c,e]*dens[a,f]  # (bca), (def), antisym last two pairs
-                         -dens[a,e]*dens[c,f]
-                         -dens[c,f]*dens[a,e]
-                         +dens[a,f]*dens[c,e] )        
-        res[c,d] += val*( dens[a,e]*dens[b,f]  # (cab), (def), antisym last two pairs
-                         -dens[b,e]*dens[a,f]
-                         -dens[a,f]*dens[b,e]
-                         +dens[b,f]*dens[a,e] )
-        res[a,e] += val*( dens[b,f]*dens[c,d]  # (abc), (efd), antisym last two pairs
-                         -dens[c,f]*dens[b,d]
-                         -dens[b,d]*dens[c,f]
-                         +dens[c,d]*dens[b,f] )
-        res[b,e] += val*( dens[c,f]*dens[a,d]  # (bca), (efd), antisym last two pairs
-                         -dens[a,f]*dens[c,d]
-                         -dens[c,d]*dens[a,f]
-                         +dens[a,d]*dens[c,f] )        
-        res[c,e] += val*( dens[a,f]*dens[b,d]  # (cab), (efd), antisym last two pairs
-                         -dens[b,f]*dens[a,d]
-                         -dens[a,d]*dens[b,f]
-                         +dens[b,d]*dens[a,f] )
-        res[a,f] += val*( dens[b,d]*dens[c,e]  # (abc), (fde), antisym last two pairs
-                         -dens[c,d]*dens[b,e]
-                         -dens[b,e]*dens[c,d]
-                         +dens[c,e]*dens[b,d] )
-        res[b,f] += val*( dens[c,e]*dens[a,f]  # (bca), (fde), antisym last two pairs
-                         -dens[a,e]*dens[c,f]
-                         -dens[c,f]*dens[a,e]
-                         +dens[a,f]*dens[c,e] )        
-        res[c,f] += val*( dens[a,d]*dens[b,e]  # (cab), (fde), antisym last two pairs
-                         -dens[b,d]*dens[a,e]
-                         -dens[a,e]*dens[b,d]
-                         +dens[b,e]*dens[a,d] )
+@jax.jit
+def contract_3nf_fused(indices: Array, values: Array, dens: Array) -> Array:
+    """Contract the sparse three-body interaction with two densities."""
+    a, b, c, d, e, f = (indices[:, i] for i in range(6))
+    n = dens.shape[0]
+    dtype = jnp.result_type(values.dtype, dens.dtype)
+    v2 = values * 2.0
+    res = jnp.zeros((n, n), dtype=dtype)
+
+    res = res.at[a, d].add(v2 * (dens[b, e] * dens[c, f] - dens[c, e] * dens[b, f]))
+    res = res.at[b, d].add(v2 * (dens[c, e] * dens[a, f] - dens[a, e] * dens[c, f]))
+    res = res.at[c, d].add(v2 * (dens[a, e] * dens[b, f] - dens[b, e] * dens[a, f]))
+
+    res = res.at[a, e].add(v2 * (dens[b, f] * dens[c, d] - dens[c, f] * dens[b, d]))
+    res = res.at[b, e].add(v2 * (dens[c, f] * dens[a, d] - dens[a, f] * dens[c, d]))
+    res = res.at[c, e].add(v2 * (dens[a, f] * dens[b, d] - dens[b, f] * dens[a, d]))
+
+    res = res.at[a, f].add(v2 * (dens[b, d] * dens[c, e] - dens[c, d] * dens[b, e]))
+    res = res.at[b, f].add(v2 * (dens[c, d] * dens[a, e] - dens[a, d] * dens[c, e]))
+    res = res.at[c, f].add(v2 * (dens[a, d] * dens[b, e] - dens[b, d] * dens[a, e]))
     return res
 
-def make_HF_ham(op1,op2,op3,dens):
-    """
-    takes Hamiltonian consisting of one-body operator op1, two-body operator op2,
-    and three-body operator op3, and the density matrix and returns the Hartree-Fock Hamiltonian.
+def build_mean_fields(
+    dens: Array,
+    v2_idx: Array,
+    v2_val: Array,
+    w3_idx: Array = None,
+    w3_val: Array = None,
+) -> tuple[Array, Array]:
+    gamma = hermitianize(contract_2nf_fused(v2_idx, v2_val, dens))
+    omega = None
+    if (w3_idx is not None) and (w3_val is not None):
+        omega = hermitianize(contract_3nf_fused(w3_idx, w3_val, dens))
+    return gamma, omega
 
-    :param op1:  list of one-body matrix elements
-    :type op1:   list[list[int,int, float]]
-    :param op2:  list of two-body matrix elements
-    :type op2:   list[list[int,int,int,,int, float]]
-    :param op3:  list of three-body matrix elements
-    :type op3:    list[list[int,int,int,int,int,int, float]]
-    :param dens: density matrix (same shape as op1)
-    :type dens:  numpy.array((:,:), dtype=float)
-    :return:     matrix in the shape of op1 and dens that is the Hartree-Fock Hamiltonian
-    :rtype:      numpy.array((:,:), dtype=float)
-    """
-    nstat = len(dens)
-    denstype=dens.dtype
-    hf_op = get_1body_matrix(op1,nstat).astype(denstype)
-    hf_op += contract_2nf(op2,dens)
-    hf_op += 0.5*contract_3nf(op3,dens)
-    return hf_op
 
-def init_density(nstat,hole,dtype=float):
-    """
-    creates a density matrix of dimension nstat x nstat given the hole information
+def build_fock(
+    h1: Array,
+    gamma: Array,
+    omega: Array = None,
+) -> Array:
+    if omega is None:
+        return hermitianize(h1 + gamma)
+    return hermitianize(h1 + gamma + 0.5 * omega)
 
-    :param nstat: dimension of single-particle basis
-    :type nstat:  int
-    :param dtype: data type of returned object
-    :type dtype:  numpy.dtype, i.e. np.float64 or float or np.complex128 or complex
-    :param hole:  tuple of occupied single-particle states, as numbers from 0 ... A-1
-    :type hole:   tuple(int, int, ... )
-    :return:      density matrix where hole states are occupied (1) and all others not (0)
-    :rtype:       numpy.array((nstat,nstat), dtype = float)
-    """
-    dens = np.zeros((nstat,nstat),dtype=dtype)
-    for i in hole:
-        dens[i,i] = 1.0
+def hf_energy(
+    dens: Array,
+    h1: Array,
+    gamma: Array,
+    omega: Array = None,
+) -> Array:
+
+    e_h1 = jnp.einsum("ij,ji->", h1, dens)
+    e_gamma = jnp.einsum("ij,ji->", gamma, dens)
+    e_omega = jnp.asarray(0, dtype=jnp.real(dens[0]).dtype)
+    if omega is not None:
+        e_omega = jnp.einsum("ij,ji->", omega, dens)
+    return jnp.real(e_h1 + 0.5 * e_gamma + (1.0 / 6.0) * e_omega)
+
+def init_density(nstat: int, hole: tuple[int], dtype=None):
+    dens = jnp.zeros((nstat, nstat), dtype=dtype)
+    hole_indices = jnp.array(hole)
+    dens = dens.at[hole_indices, hole_indices].set(1.0)
     return dens
 
-def HF_iter(op1, op2, op3, dens, mix=0.5):
-    """
-    Performs one iteration of the Hartree-Fock procedure
+@partial(jax.jit, static_argnames=("npart", "diagonalizer"))
+def _scf_step(
+    dens, h1, v2_idx, v2_val, w3_idx, w3_val, npart, mix, prev_vecs,
+    diagonalizer,
+):
+    gamma, omega = build_mean_fields(dens, v2_idx, v2_val, w3_idx, w3_val)
+    fock = build_fock(h1, gamma, omega)
+    energy = hf_energy(dens, h1, gamma, omega)
 
-    :param op1:  list of one-body matrix elements
-    :type op1:   list[list[int,int, float]]
-    :param op2:  list of two-body matrix elements
-    :type op2:   list[list[int,int,int,,int, float]]
-    :param op3:  list of three-body matrix elements
-    :type op3:   list[list[int,int,int,int,int,int, float]]
-    :param dens: density matrix (same shape as op1)
-    :type dens:  numpy.array((:,:), dtype=float)
-    :param mix:  returned density matrix is mix*new_density + (1-mix)*old_density
-    :type mix:   float 
-    :return:     energy, density, vecs as the current HF energy, current density
-                 matrix, and orthogonal transformation matrix that diagonalizes
-                 the HF Hamiltonian
-    :rtype:      float, numpy.array((:,:), dtype=float), numpy.array((:,:), dtype=float)
-    """
-    npart=round(np.real(np.trace(dens))) # rounds to nearest integer
-    erg = HF_energy(op1, op2, op3, dens)
-    hf = make_HF_ham(op1, op2, op3, dens)
-    vals, vecs = np.linalg.eigh(hf)
-    new_dens=contract("pi,qi->pq", vecs[:,0:npart], np.conjugate(vecs[:,0:npart]) )
-    res_dens = mix*new_dens + (1.0-mix)*dens
-    return erg, res_dens, vecs
+    if diagonalizer == "dense":
+        _, orbitals = jnp.linalg.eigh(fock)
+        occ = orbitals[:, :npart]
+    else:
+        _, occ = _occupied_orbitals(fock, npart, prev_vecs)
 
-def solve_HF(op1, op2, op3, dens, mix=0.5, eps=1.e-8, max_iter=100, verbose=False):
-    """
-    Solve the Hartree-Fock problem
+    new_density = occ @ _adjoint(occ)
 
-    :param op1:  list of one-body matrix elements
-    :type op1:   list[list[int,int, float]]
-    :param op2:  list of two-body matrix elements
-    :type op2:   list[list[int,int,int,,int, float]]
-    :param op3:  list of three-body matrix elements
-    :type op3:   list[list[int,int,int,int,int,int, float]]
-    :param dens: density matrix (same shape as op1)
-    :type dens:  numpy.array((:,:), dtype=float)
-    :param mix:  parameter used in the mixing: mix*new_density + (1-mix)*old_density
-    :type mix:   float
-    :param eps:  converegence of energy
-    :type eps:   float
-    :param max_iter:  maximum number of HF iterations
-    :type max_iter:   float
-    :return:     energy, orthogonal transformation matrix that diagonalizes
-                 the HF Hamiltonian (the first A columns are occupied), converged
-    :rtype:      float, numpy.array((:,:), dtype=float), boolean
-    """
+    residual_density = jnp.sum(jnp.abs(new_density - dens))
+    mixed_density = (1.0 - mix) * dens + mix * new_density
+
+    return occ, energy, mixed_density, residual_density
+
+def prepare_inputs(op1, op2, op3, dens, dtype=jnp.float64):
+    has_three_body = op3 is not None and len(op3) > 0
+
+    h1 = jnp.asarray(op1.to_dense())
+    v2_idx = jnp.asarray(op2.indices)
+    v2_val = jnp.asarray(op2.values)
+    if has_three_body:
+        w3_idx = jnp.asarray(op3.indices)
+        w3_val = jnp.asarray(op3.values)
+    else:
+        w3_idx = None
+        w3_val = None
+    dens = jnp.asarray(dens)
+
+    return h1, v2_idx, v2_val, w3_idx, w3_val, dens
+
+def solve_HF(
+    op1: OneBodyOperator,
+    op2: TwoBodyOperator,
+    op3: ThreeBodyOperator,
+    dens,
+    mix=0.5,
+    eps=1e-8,
+    max_iter=100,
+    verbose=False,
+    diagonalizer="davidson",
+):
+
+    if diagonalizer not in {"davidson", "dense"}:
+        raise ValueError("diagonalizer must be 'davidson' or 'dense'")
+
+    h1_dense, v2_idx, v2_val, w3_idx, w3_val, _dens = prepare_inputs(
+        op1, op2, op3, dens, dtype=jnp.float64
+    )
+
+    prev_energy = 0.0
     converged = False
-    my_dens=dens.copy()
-    erg0 = HF_energy(op1, op2, op3, my_dens)
+    npart = int(jnp.real(jnp.trace(_dens)).round())
+
+    occ = occupied_orbitals_from_diagonal_density(_dens, npart)
+
     for i in range(max_iter):
-        erg, new_dens, vecs = HF_iter(op1, op2, op3, my_dens, mix=mix)
-        diff = np.abs(erg-erg0)
-        diff_dens = np.sum(np.abs(new_dens-my_dens))
+        occ, energy, _dens, diff_dens = _scf_step(
+            _dens, h1_dense, v2_idx, v2_val, w3_idx, w3_val, npart, mix, occ,
+            diagonalizer,
+        )
+
+        dE = jnp.abs(energy - prev_energy)
+
         if verbose:
-            print(i, "E=", erg, ", Delta E=", diff, ", Delta rho =", diff_dens)
-        if diff_dens < eps and i > 1:
+            # convert to jax debug logging
+            print(f"Iter {i}: E={energy:.8f}, dE={dE:.6e}, dRho={diff_dens:.6e}")
+
+        if (diff_dens < eps or dE < eps) and i > 1:
             converged = True
             break
-        else:
-            erg0 = erg
-            my_dens = new_dens.copy()
-    return erg, vecs, converged
 
-def HF_energy(op1, op2, op3, dens, w3_sparse=False):
-    """
-    Computes the Hartree-Fock energy for a given density dens and Hamiltonian consisting
-    of one-body term op1, two-body term op2, and three-body term op3
-    :param op1:  list of one-body matrix elements
-    :type op1:   list[list[int,int, float]]
-    :param op2:  list of two-body matrix elements
-    :type op2:   list[list[int,int,int,int, float]]
-    :param op3:  list of three-body matrix elements
-    :type op3:   list[list[int,int,int,int,int,int, float]]
-    :param dens: density matrix (same shape as op1)
-    :type dens:  numpy.array((:,:), dtype=float)
-    :param w3_sparse: 
-    :return:     Hartree-Fock energy
-    :rtype:      float
-    """
-    nstat = len(dens)
-    data_type=dens.dtype
-    dum = get_1body_matrix(op1,nstat,dtype=data_type)
-    dum += 0.5*contract_2nf(op2,dens)
-    if w3_sparse:
-        dum += (1.0/6.0)*contract_3nf_sparse(op3,dens)
-    else:
-        dum += (1.0/6.0)*contract_3nf(op3,dens)
-        
-    erg = contract("ij,ji",dum,dens)
-    return np.real_if_close(erg)
+        prev_energy = energy
 
-def contract_3nf_sparse(csr,dens):
-    """
-    takes list of three-body matrix elements and contracts them with the density to get a one-body operator
-    :param csr:  matrix of three-body elements
-    :type csr:   scipy.sparse.csr_array
-    :param dens: square density matrix
-    :type dens:  numpy.array((:,:), dtype=float)
-    :return:     one-body operator of the same shape as the density matrix dens
-    :rtype:      numpy.array((:,:), dtype=float)
-    """
-    dim = len(dens)
-    res = np.zeros_like(dens)
-    rows, cols = csr.shape
-    for i in range(rows):
-    # Get start and end indices for current row i
-        start = csr.indptr[i]
-        end = csr.indptr[i+1]
+    return energy, occ, converged
 
-        row_values = csr.data[start:end]
-        row_indices = csr.indices[start:end]
-
-        for j, val in zip(row_indices, row_values):
-            # process row i, col j
-            # index = a + b * dim + c*dim**2
-            a = i % dim
-            b = ((i - a) % dim**2) // dim
-            c = (i - a - b*dim) // dim**2
-            d = j % dim
-            e = ((j - d) % dim**2) // dim
-            f = (j - d - e*dim) // dim**2
-            res[a,d] += val*( dens[b,e]*dens[c,f]  # (abc), (def), antisym last two pairs
-                             -dens[c,e]*dens[b,f]
-                             -dens[b,f]*dens[c,e]
-                             +dens[c,f]*dens[b,e] )
-            res[b,d] += val*( dens[c,e]*dens[a,f]  # (bca), (def), antisym last two pairs
-                             -dens[a,e]*dens[c,f]
-                             -dens[c,f]*dens[a,e]
-                             +dens[a,f]*dens[c,e] )        
-            res[c,d] += val*( dens[a,e]*dens[b,f]  # (cab), (def), antisym last two pairs
-                             -dens[b,e]*dens[a,f]
-                             -dens[a,f]*dens[b,e]
-                             +dens[b,f]*dens[a,e] )
-            res[a,e] += val*( dens[b,f]*dens[c,d]  # (abc), (efd), antisym last two pairs
-                             -dens[c,f]*dens[b,d]
-                             -dens[b,d]*dens[c,f]
-                             +dens[c,d]*dens[b,f] )
-            res[b,e] += val*( dens[c,f]*dens[a,d]  # (bca), (efd), antisym last two pairs
-                             -dens[a,f]*dens[c,d]
-                             -dens[c,d]*dens[a,f]
-                             +dens[a,d]*dens[c,f] )        
-            res[c,e] += val*( dens[a,f]*dens[b,d]  # (cab), (efd), antisym last two pairs
-                             -dens[b,f]*dens[a,d]
-                             -dens[a,d]*dens[b,f]
-                             +dens[b,d]*dens[a,f] )
-            res[a,f] += val*( dens[b,d]*dens[c,e]  # (abc), (fde), antisym last two pairs
-                             -dens[c,d]*dens[b,e]
-                             -dens[b,e]*dens[c,d]
-                             +dens[c,e]*dens[b,d] )
-            res[b,f] += val*( dens[c,e]*dens[a,f]  # (bca), (fde), antisym last two pairs
-                             -dens[a,e]*dens[c,f]
-                             -dens[c,f]*dens[a,e]
-                             +dens[a,f]*dens[c,e] )        
-            res[c,f] += val*( dens[a,d]*dens[b,e]  # (cab), (fde), antisym last two pairs
-                             -dens[b,d]*dens[a,e]
-                             -dens[a,e]*dens[b,d]
-                             +dens[b,e]*dens[a,d] )
-    return res
+def occupied_orbitals_from_diagonal_density(dens: jax.Array, npart: int) -> jax.Array:
+    indices = jnp.argsort(jnp.real(jnp.diag(dens)))[-npart:]
+    return dens[:, indices]
---
+++ NuLattice/utils/_jax_types.py
@@ -0,0 +1,225 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+from jax.experimental.sparse import BCOO
+from jax.sharding import NamedSharding, PartitionSpec as P
+
+class Operator:
+    def __init__(self, indices: jnp.ndarray, values: jnp.ndarray, nstat: int):
+        self.nstat = nstat
+        # JAX arrays are the primary backend here
+        self.indices = jnp.asarray(indices, dtype=jnp.int32)
+        self.values = jnp.asarray(values)
+
+        if self.indices.ndim == 1:
+            self.indices = self.indices[:, jnp.newaxis]
+
+    def __len__(self):
+        return len(self.values)
+
+    def to_list(self):
+        if len(self) == 0:
+            return []
+
+        out_list = []
+        for i in range(len(self.values)):
+            row = self.indices[i].tolist()
+            row.append(self.values[i])
+            out_list.append(row)
+        return out_list
+
+    def to_bcoo(self, mesh=None):
+        """Converts operator to a JAX BCOO sparse array, optionally sharded."""
+        rank = self._get_expected_rank()
+        shape = (self.nstat,) * rank
+
+        # If a mesh is provided, shard the NNZ dimension
+        if mesh:
+            sharding = NamedSharding(mesh, P("data"))
+            indices = jax.device_put(self.indices, sharding)
+            data = jax.device_put(self.values, sharding)
+            return BCOO((data, indices), shape=shape)
+
+        return BCOO((self.values, self.indices), shape=shape)
+
+    def to_dense(self, mesh=None):
+        rank = self.indices.shape[1]
+        shape = (self.nstat,) * rank
+        mat = jnp.zeros(shape, dtype=self.values.dtype)
+        mat = mat.at[tuple(self.indices[:, i] for i in range(rank))].add(self.values)
+        if mesh:
+            sharding = NamedSharding(mesh, P("data", *((None,) * (rank - 1))))
+            return jax.device_put(mat, sharding)
+        return mat
+
+    @classmethod
+    def from_list(
+        cls,
+        operator_list,
+        nstat: int,
+    ):
+        """Operator from a legacy list of lists [[p, q, ..., val], ...]"""
+        if not operator_list:
+            rank = cls._get_expected_rank()
+            return cls(
+                jnp.empty((0, rank), dtype=jnp.int32),
+                jnp.empty((0,), dtype=jnp.float64),
+                nstat,
+            )
+
+        data = jnp.array(operator_list, dtype=jnp.float64)
+        indices = jnp.round(data[:, :-1]).astype(jnp.int32)
+        values = data[:, -1]
+
+        return cls(indices, values, nstat)
+
+
+
+class OneBodyOperator(Operator):
+    def __init__(self, indices, values, nstat):
+        super().__init__(indices, values, nstat)
+        if len(self) > 0 and self.indices.shape[1] != 2:
+            raise ValueError(f"Expected (N, 2) indices, got {self.indices.shape}")
+
+    @classmethod
+    def _get_expected_rank(cls):
+        return 2
+
+class TwoBodyOperator(Operator):
+    def __init__(self, indices, values, nstat):
+        super().__init__(indices, values, nstat)
+        if len(self) > 0 and self.indices.shape[1] != 4:
+            raise ValueError(f"Expected (N, 4) indices, got {self.indices.shape}")
+
+    @classmethod
+    def _get_expected_rank(cls):
+        return 4
+
+    @classmethod
+    def from_sparse(
+        cls,
+        matrix,
+        nstat: int,
+    ):
+        p = matrix.row % nstat
+        q = matrix.row // nstat
+        r = matrix.col % nstat
+        s = matrix.col // nstat
+
+        indices = np.column_stack((p, q, r, s)).astype(np.int32, copy=False)
+
+        return cls(indices, matrix.data, nstat)
+
+    @classmethod
+    def from_scipy_csr(
+        cls,
+        matrix,
+        nstat: int,
+    ):
+        matrix = matrix.tocoo()
+        return cls.from_scipy_csr(matrix, nstat)
+
+class ThreeBodyOperator(Operator):
+    def __init__(self, indices, values, nstat):
+        super().__init__(indices, values, nstat)
+        if len(self) > 0 and self.indices.shape[1] != 6:
+            raise ValueError(f"Expected (N, 6) indices, got {self.indices.shape}")
+
+    @classmethod
+    def _get_expected_rank(cls):
+        return 6
+
+    @classmethod
+    def from_scipy_coo(matrix, nstat):
+        nstat2 = nstat * nstat
+
+        p = matrix.row % nstat
+        q = (matrix.row // nstat) % nstat
+        r = matrix.row // nstat2
+        s = matrix.col % nstat
+        t = (matrix.col // nstat) % nstat
+        u = matrix.col // nstat2
+
+        indices = np.column_stack((p, q, r, s, t, u)).astype(np.int32, copy=False)
+
+        return ThreeBodyOperator(indices, matrix.data, nstat)
+
+    def from_scipy_csr(cls, matrix, nstat):
+        matrix = matrix.tocoo()
+        return cls.from_scipy_coo(matrix, nstat)
+
+class ShardingManager:
+    def __init__(self, num_nodes=1, num_gpus=1):
+        self.num_nodes = num_nodes
+        self.num_gpus = num_gpus
+        self.mesh = jax.make_mesh(axis_shapes=(num_nodes, num_gpus), axis_names=("nodes", "gpus"))
+
+    def prepare(self, arr, rank: int | None = None, spec: NamedSharding = None):
+        r = rank if rank is not None else arr.ndim 
+
+        if spec is not None:
+            spec = spec
+        else:
+            if r == 0: 
+                spec = P() # alternatively can be used for replication
+            elif r == 1:
+                spec = P(('nodes', 'gpus')) # 1d array should be split across everything
+            else:
+                if self.num_nodes == 1 or self.num_gpus == 1:
+                    spec = P(("nodes", "gpus"), *([None] * (r - 1)))
+                else:
+                    spec = P("nodes", "gpus", *([None] * (r - 2)))
+
+        sharding = NamedSharding(self.mesh, spec)
+
+        # cpu check
+        if isinstance(arr, np.ndarray):
+            if arr.nbytes > 1e9: # only if > 1 gb
+                # calculate bounding box per gpu, slice on cpu, move to gpu
+                return jax.make_array_from_callback(
+                    arr.shape,
+                    sharding,
+                    lambda idx: arr[idx])
+            else:
+                return jax.device_put(arr, sharding)
+        return jax.device_put(arr, sharding)
+
+def two_body_from_sparse(matrix, nstat):
+    matrix = matrix.tocoo()
+
+    p = matrix.row % nstat
+    q = matrix.row // nstat
+    r = matrix.col % nstat
+    s = matrix.col // nstat
+    indices = np.column_stack((p, q, r, s)).astype(np.int32, copy=False)
+
+    return TwoBodyOperator(indices, matrix.data, nstat)
+
+def three_body_from_sparse(matrix, nstat):
+
+    matrix = matrix.tocoo()
+    nstat2 = nstat * nstat
+
+    p = matrix.row % nstat
+    q = (matrix.row // nstat) % nstat
+    r = matrix.row // nstat2
+    s = matrix.col % nstat
+    t = (matrix.col // nstat) % nstat
+    u = matrix.col // nstat2
+    indices = np.column_stack((p, q, r, s, t, u)).astype(np.int32, copy=False)
+
+    return ThreeBodyOperator(indices, matrix.data, nstat)
+
+def one_body_from_list(elements, nstat):
+
+    if len(elements) == 0:
+        return OneBodyOperator(
+            np.empty((0, 2), dtype=np.int32),
+            np.empty(0, dtype=np.float64),
+            nstat,
+        )
+
+    indices = np.asarray([row[:2] for row in elements], dtype=np.int32)
+    values = np.asarray([row[2] for row in elements])
+    return OneBodyOperator(indices, values, nstat)
