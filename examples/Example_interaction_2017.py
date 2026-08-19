17dca39d1bd19a4683581e0053a2379852173181
Matthias Heinz <heinzmc@ornl.gov>
2026-08-18 21:07:13 -0400

    Make 2017 example work again

--- examples/Example_interaction_2017.py
+++ examples/Example_interaction_2017.py
@@ -6,6 +6,7 @@ import NuLattice.lattice as lat
 import NuLattice.constants_NLEFT as nleftConsts
 import NuLattice.HF.hartree_fock as hf
 import NuLattice.references as ref
+import NuLattice.operators.jax_adapters as jax_help
 if __name__ == '__main__':
     thisL = 4
     a = 1.0 / 100.0
@@ -37,9 +38,12 @@ if __name__ == '__main__':
 
     eps=1.e-8
     mix = 0.7
-    max_iter=100
+    max_iter=500
     verbose = True
-    erg, trafo, conv = hf.solve_HF(myTkin, my_VNN, [], dens,
+    myTkin = jax_help.one_body_from_list(myTkin, len(my_basis))
+    my_VNN = jax_help.two_body_from_sparse(v_0 + v_OPE, len(my_basis))
+    my_V3N = jax_help.empty_three_body(len(my_basis))
+    erg, trafo, conv = hf.solve_HF(myTkin, my_VNN, my_V3N, dens,
                                 mix=mix, eps=eps, max_iter=max_iter, verbose=verbose)
 
     if conv:
