4d74aa880bdd8effd15d80714d4c02ffe49518d3
Matthias Heinz <heinzmc@ornl.gov>
2026-08-20 10:01:57 -0400

    Simple renaming of variables

--- NuLattice/HF/hartree_fock.py
+++ NuLattice/HF/hartree_fock.py
@@ -17,59 +17,59 @@ def _adjoint(x):
 def _make_hermitian(x):
     return 0.5 * (x + _adjoint(x))
 
-def init_density(nstat: int, hole: Tuple[int], dtype=None):
-    dens = jnp.zeros((nstat, nstat), dtype=dtype)
-    hole_indices = jnp.array(hole)
-    dens = dens.at[hole_indices, hole_indices].set(1.0)
-    return dens
+def init_density(number_of_states: int, hole_indices: Tuple[int], dtype=None):
+    density = jnp.zeros((number_of_states, number_of_states), dtype=dtype)
+    hole_indices = jnp.array(hole_indices)
+    density = density.at[hole_indices, hole_indices].set(1.0)
+    return density
 
 @jax.jit
-def contract_2nf_fused(indices: Array, values: Array, dens: Array) -> Array:
+def contract_2nf_fused(indices: Array, values: Array, density: Array) -> Array:
     """Contract the sparse two-body interaction with a one-body density."""
     p, q, r, s = (indices[:, i] for i in range(4))
-    n = dens.shape[0]
-    dtype = jnp.result_type(values.dtype, dens.dtype)
+    n = density.shape[0]
+    dtype = jnp.result_type(values.dtype, density.dtype)
     res = jnp.zeros((n, n), dtype=dtype)
-    res = res.at[p, r].add(+values * dens[q, s])
-    res = res.at[q, r].add(-values * dens[p, s])
-    res = res.at[p, s].add(-values * dens[q, r])
-    res = res.at[q, s].add(+values * dens[p, r])
+    res = res.at[p, r].add(+values * density[q, s])
+    res = res.at[q, r].add(-values * density[p, s])
+    res = res.at[p, s].add(-values * density[q, r])
+    res = res.at[q, s].add(+values * density[p, r])
     return res
 
 
 @jax.jit
-def contract_3nf_fused(indices: Array, values: Array, dens: Array) -> Array:
+def contract_3nf_fused(indices: Array, values: Array, density: Array) -> Array:
     """Contract the sparse three-body interaction with two densities."""
     a, b, c, d, e, f = (indices[:, i] for i in range(6))
-    n = dens.shape[0]
-    dtype = jnp.result_type(values.dtype, dens.dtype)
+    n = density.shape[0]
+    dtype = jnp.result_type(values.dtype, density.dtype)
     v2 = values * 2.0
     res = jnp.zeros((n, n), dtype=dtype)
 
-    res = res.at[a, d].add(v2 * (dens[b, e] * dens[c, f] - dens[c, e] * dens[b, f]))
-    res = res.at[b, d].add(v2 * (dens[c, e] * dens[a, f] - dens[a, e] * dens[c, f]))
-    res = res.at[c, d].add(v2 * (dens[a, e] * dens[b, f] - dens[b, e] * dens[a, f]))
+    res = res.at[a, d].add(v2 * (density[b, e] * density[c, f] - density[c, e] * density[b, f]))
+    res = res.at[b, d].add(v2 * (density[c, e] * density[a, f] - density[a, e] * density[c, f]))
+    res = res.at[c, d].add(v2 * (density[a, e] * density[b, f] - density[b, e] * density[a, f]))
 
-    res = res.at[a, e].add(v2 * (dens[b, f] * dens[c, d] - dens[c, f] * dens[b, d]))
-    res = res.at[b, e].add(v2 * (dens[c, f] * dens[a, d] - dens[a, f] * dens[c, d]))
-    res = res.at[c, e].add(v2 * (dens[a, f] * dens[b, d] - dens[b, f] * dens[a, d]))
+    res = res.at[a, e].add(v2 * (density[b, f] * density[c, d] - density[c, f] * density[b, d]))
+    res = res.at[b, e].add(v2 * (density[c, f] * density[a, d] - density[a, f] * density[c, d]))
+    res = res.at[c, e].add(v2 * (density[a, f] * density[b, d] - density[b, f] * density[a, d]))
 
-    res = res.at[a, f].add(v2 * (dens[b, d] * dens[c, e] - dens[c, d] * dens[b, e]))
-    res = res.at[b, f].add(v2 * (dens[c, d] * dens[a, e] - dens[a, d] * dens[c, e]))
-    res = res.at[c, f].add(v2 * (dens[a, d] * dens[b, e] - dens[b, d] * dens[a, e]))
+    res = res.at[a, f].add(v2 * (density[b, d] * density[c, e] - density[c, d] * density[b, e]))
+    res = res.at[b, f].add(v2 * (density[c, d] * density[a, e] - density[a, d] * density[c, e]))
+    res = res.at[c, f].add(v2 * (density[a, d] * density[b, e] - density[b, d] * density[a, e]))
     return res
 
 def build_2b_and_3b_fock_matrices(
-    dens: Array,
+    density: Array,
     v2_idx: Array,
     v2_val: Array,
     w3_idx: Array = None,
     w3_val: Array = None,
 ) -> tuple[Array, Array]:
-    fock_2b = _make_hermitian(contract_2nf_fused(v2_idx, v2_val, dens))
+    fock_2b = _make_hermitian(contract_2nf_fused(v2_idx, v2_val, density))
     fock_3b = None
     if (w3_idx is not None) and (w3_val is not None):
-        fock_3b = _make_hermitian(contract_3nf_fused(w3_idx, w3_val, dens))
+        fock_3b = _make_hermitian(contract_3nf_fused(w3_idx, w3_val, density))
     return fock_2b, fock_3b
 
 
@@ -83,51 +83,51 @@ def build_full_fock_matrix(
     return _make_hermitian(h1 + fock_2b + 0.5 * fock_3b)
 
 def compute_hf_energy_from_fock_matrices(
-    dens: Array,
+    density: Array,
     h1: Array,
     fock_2b: Array,
     fock_3b: Array = None,
 ) -> Array:
 
-    e_h1 = jnp.einsum("ij,ji->", h1, dens)
-    e_2b = jnp.einsum("ij,ji->", fock_2b, dens)
-    e_3b = jnp.asarray(0, dtype=jnp.real(dens[0]).dtype)
+    e_h1 = jnp.einsum("ij,ji->", h1, density)
+    e_2b = jnp.einsum("ij,ji->", fock_2b, density)
+    e_3b = jnp.asarray(0, dtype=jnp.real(density[0]).dtype)
     if fock_3b is not None:
-        e_3b = jnp.einsum("ij,ji->", fock_3b, dens)
+        e_3b = jnp.einsum("ij,ji->", fock_3b, density)
     return jnp.real(e_h1 + 0.5 * e_2b + (1.0 / 6.0) * e_3b)
 
 
 
 @partial(jax.jit, static_argnames=("npart", "diagonalizer", ))
 def iterate_hf_equations(
-    dens, h1, v2_idx, v2_val, w3_idx, w3_val, npart, mix, prev_vecs,
+    density, h1, v2_idx, v2_val, w3_idx, w3_val, number_of_particles, mixing_param, prev_vecs,
     diagonalizer, davidson_max_iter
 ):
-    fock_2b, fock_3b = build_2b_and_3b_fock_matrices(dens, v2_idx, v2_val, w3_idx, w3_val)
+    fock_2b, fock_3b = build_2b_and_3b_fock_matrices(density, v2_idx, v2_val, w3_idx, w3_val)
     fock = build_full_fock_matrix(h1, fock_2b, fock_3b)
-    energy = compute_hf_energy_from_fock_matrices(dens, h1, fock_2b, fock_3b)
+    energy = compute_hf_energy_from_fock_matrices(density, h1, fock_2b, fock_3b)
 
     _, orbitals = (
         jnp.linalg.eigh(fock)
         if diagonalizer == "dense"
-        else davidson_eigh(fock, npart, prev_vecs, davidson_max_iter)
+        else davidson_eigh(fock, number_of_particles, prev_vecs, davidson_max_iter)
     )
-    occupied_orbitals = orbitals[:, :npart]
+    occupied_orbitals = orbitals[:, :number_of_particles]
 
     new_density = occupied_orbitals @ _adjoint(occupied_orbitals)
 
-    mixed_density = (1.0 - mix) * dens + mix * new_density
-    residual_density = jnp.sum(jnp.abs(mixed_density - dens))
+    mixed_density = (1.0 - mixing_param) * density + mixing_param * new_density
+    residual_density = jnp.sum(jnp.abs(mixed_density - density))
 
     return occupied_orbitals, energy, mixed_density, residual_density
 
-def prepare_inputs(op1, op2, op3, dens: Array, sm: ShardingManager, dtype=jnp.float64):
+def prepare_inputs(op1, op2, op3, density: Array, sm: ShardingManager, dtype=jnp.float64):
     has_three_body = op3 is not None and len(op3) > 0
 
     if sm is not None:
         assert sm.num_nodes == 1 or sm.num_gpus == 1, "HF expects 1D mesh, ensure sm.num_nodes or sm.num_gpus is 1"
         h1 = sm.prepare(op1.to_dense(), rank=0)
-        dens = sm.prepare(dens, rank=0)
+        density = sm.prepare(density, rank=0)
         v2_idx = sm.prepare(op2.indices)
         v2_val = sm.prepare(op2.values)
         if has_three_body:
@@ -146,15 +146,15 @@ def prepare_inputs(op1, op2, op3, dens: Array, sm: ShardingManager, dtype=jnp.fl
         else:
             w3_idx = None
             w3_val = None
-        dens = jnp.asarray(dens)
+        density = jnp.asarray(density)
 
-    return h1, v2_idx, v2_val, w3_idx, w3_val, dens
+    return h1, v2_idx, v2_val, w3_idx, w3_val, density
 
 def solve_HF(
     op1,
     op2,
     op3,
-    dens: Array,
+    density: Array,
     mix: float =0.5,
     eps: float =1e-8,
     max_iter: int = 100,
@@ -168,35 +168,35 @@ def solve_HF(
     if diagonalizer not in {"davidson", "dense"}:
         raise ValueError("diagonalizer must be 'davidson' or 'dense'")
 
-    h1_dense, v2_idx, v2_val, w3_idx, w3_val, _dens = prepare_inputs(
-        op1, op2, op3, dens, sm
+    h1_dense, v2_idx, v2_val, w3_idx, w3_val, _density = prepare_inputs(
+        op1, op2, op3, density, sm
     )
 
     prev_energy = 0.0
     converged = False
-    npart = int(jnp.real(jnp.trace(_dens)).round())
+    npart = int(jnp.real(jnp.trace(_density)).round())
 
-    occupied_orbitals = guess_occupied_orbitals_from_density(_dens, npart)
+    occupied_orbitals = guess_occupied_orbitals_from_density(_density, npart)
 
     for i in range(max_iter):
-        occupied_orbitals, energy, _dens, diff_dens = iterate_hf_equations(
-            _dens, h1_dense, v2_idx, v2_val, w3_idx, w3_val, npart, mix, occupied_orbitals,
+        occupied_orbitals, energy, _density, diff_density = iterate_hf_equations(
+            _density, h1_dense, v2_idx, v2_val, w3_idx, w3_val, npart, mix, occupied_orbitals,
             diagonalizer, davidson_max_iter,
         )
 
         dE = jnp.abs(energy - prev_energy)
 
         if verbose:
-            print(f"Iter {i}: E={energy:.8f}, dE={dE:.6e}, dRho={diff_dens:.6e}")
+            print(f"Iter {i}: E={energy:.8f}, dE={dE:.6e}, dRho={diff_density:.6e}")
 
-        if (diff_dens < eps):
+        if (diff_density < eps):
             converged = True
             break
 
         prev_energy = energy
 
     if keep_all_orbitals:
-        fock_2b, fock_3b = build_2b_and_3b_fock_matrices(_dens, v2_idx, v2_val, w3_idx, w3_val)
+        fock_2b, fock_3b = build_2b_and_3b_fock_matrices(_density, v2_idx, v2_val, w3_idx, w3_val)
         fock = build_full_fock_matrix(h1_dense, fock_2b, fock_3b)
         _, orbs = jnp.linalg.eigh(fock)
     else:
